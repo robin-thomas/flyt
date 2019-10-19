8a1927924a7e68cdaceca2e518b83adb11b057ea
Robin Thomas <robinthomas2591@gmail.com>
2019-10-19 14:40:12 +0800

    Update the now.json with api callbacks

--- src/api/server.js
+++ src/api/server.js
@@ -8,19 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.post(config.flyt.api.createNewPolicy, async (req, res) => {
+  const policy = req.body.policy;
+
+  await Contract.invokeFn("createNewPolicy", policy);
+
+  res.status(200).send();
+});
+
 app.post(config.flyt.api.callback, async (req, res) => {
   const policyId = Number(req.body.policyId);
   const txHash = req.body.tx;
 
   await Contract.invokeFn("updateTxHash", policyId, txHash);
-
-  res.status(200).send();
-});
-
-app.post(config.flyt.api.createNewPolicy, async (req, res) => {
-  const policy = req.body.policy;
-
-  await Contract.invokeFn("createNewPolicy", policy);
 
   res.status(200).send();
 });
