ccb7cc99cd8b183f70823744fab58992ce31cb6a
Robin Thomas <robin@propertyguru.com.sg>
2019-10-25 11:05:02 +0700

    Add support to create policy document

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -2,13 +2,28 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 
+import PolicyPdf from "../components/PolicyPdf";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Stepper from "../components/stepper";
 import EmptyRow from "../components/utils/EmptyRow";
 
+const policy = {
+  policyId: "12345",
+  products: [],
+  flight: {
+    from: "Singapore Airport",
+    to: "Cochin Airport",
+    departureDate: "2019-10-22T00:00:00.000",
+    arrivalDate: "2019-10-22T00:00:00.000",
+    name: "MI",
+    code: "468",
+  },
+};
+
 const App = () => (
   <div className="App">
+    <PolicyPdf policy={policy}/>
     <Header />
     <div className="stepper-form-container">
       <Container>
--- src/client/components/PolicyPdf.js
+++ src/client/components/PolicyPdf.js
@@ -1,33 +1,109 @@
 import React from "react";
 
+import { Spinner } from "react-bootstrap";
+
 import {
   Page,
   Text,
   View,
   Document,
   StyleSheet,
-  Image
+  Font,
+  PDFDownloadLink
 } from "@react-pdf/renderer";
 
 import config from "../../config.json";
 
+Font.register({ family: "Roboto", fonts: [
+  { src: "http://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf" },
+  { src: "http://fonts.gstatic.com/s/robotocondensed/v14/Zd2E9abXLFGSr9G3YK2MsDR-eWpsHSw83BRsAQElGgc.ttf", fontWeight: 900 },
+]});
+
 const styles = StyleSheet.create({
-  appName: {
-    textDecoration: "uppercase",
+  page: {
+    fontFamily: "Roboto",
+  },
+  header: {
+    padding: "10% 0",
     textAlign: "center",
-    fontSize: "25px",
+    fontSize: 25,
     fontWeight: "900"
+  },
+  container: {
+    fontSize: 12,
+    padding: "0% 10%",
+  },
+  policyContainer: {
+    padding: "10% 0",
+  },
+  policy: {
+    padding: "1%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "stretch",
+  },
+  policyHeader: {
+    fontWeight: 900,
+    width: "40%",
+    textAlign: "left",
+  },
+  policyBody: {
+    width: "60%",
+    textAlign: "left",
   }
 });
 
-const PolicyPdf = ({ policy }) => (
+const Policy = ({ policy }) => (
   <Document>
-    <Page size="A4">
+    <Page size="A4" style={styles.page}>
       <View>
-        <Text style={styles.appName}>{config.app.name}</Text>
+        <Text style={styles.header}>Certificate of Insurance</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>
+          This policy is issued as a matter of information only and confers no
+          rights upon the certificate holder. This certificate does not amend, extend
+          or alter the coverage afforded by the policy detailed below, or assume
+          continuity of the policy.
+        </Text>
+        <View style={styles.policyContainer}>
+          <View style={styles.policy}>
+            <Text style={styles.policyHeader}>Insured by:</Text>
+            <Text style={styles.policyBody}>{config.app.name.toUpperCase()}</Text>
+          </View>
+          <View style={styles.policy}>
+            <Text style={styles.policyHeader}>Policy Type:</Text>
+            <Text style={styles.policyBody}>Flight Insurance</Text>
+          </View>
+          <View style={styles.policy}>
+            <Text style={styles.policyHeader}>Policy No:</Text>
+            <Text style={styles.policyBody}>{policy.policyId}</Text>
+          </View>
+          <View style={styles.policy}>
+            <Text style={styles.policyHeader}>Flight:</Text>
+            <Text style={styles.policyBody}>
+              {policy.flight.name} {policy.flight.code} from {policy.flight.from} ({policy.flight.departureDate})
+            </Text>
+          </View>
+          <View style={styles.policy}>
+            <Text style={styles.policyHeader}>Insured against:</Text>
+            <Text style={styles.policyBody}>{policy.products.join(" ,")}</Text>
+          </View>
+        </View>
+        <Text>
+          Subject to the Terms and Conditions of this Policy.
+        </Text>
       </View>
     </Page>
   </Document>
+);
+
+const PolicyPdf = ({ policy }) => (
+  <PDFDownloadLink document={<Policy policy={policy}/>} fileName={`policy-${policy.policyId}.pdf`}>
+    {({ loading }) => loading ? (
+      <Spinner animation="border" size="sm" role="status" />
+    ) : 'Download'}
+  </PDFDownloadLink>
 );
 
 export default PolicyPdf;
