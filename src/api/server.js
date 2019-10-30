ad58ce44d195d411a394afe40644a2995ba0a902
Robin Thomas <robinthomas2591@gmail.com>
2019-10-30 19:44:09 +0800

    Add swagger docs for the server apis

--- src/api/server.js
+++ src/api/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const _ = require("lodash");
 const LZUTF8 = require("lzutf8");
+const swaggerUi = require("swagger-ui-express");
 
 const parseISO = require("date-fns/parseISO");
 const format = require("date-fns/format");
 const parse = require("date-fns/parse");
 
+const Swagger = require("./swagger");
 const Flyt = require("./flyt");
 const Utils = require("./utils");
 const Contract = require("./contract");
@@ -19,6 +21,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+/**
+ * @swagger
+ * /callback:
+ *   post:
+ *     description: This api is triggered after payment is completed
+ */
 app.post(config.app.api.callback.path, async (req, res) => {
   let policy = decodeURIComponent(req.body.policy);
   policy = LZUTF8.decompress(policy, {inputEncoding: "Base64"});
@@ -85,6 +93,25 @@ app.post(config.app.api.callback.path, async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /policy/policyId:
+ *   get:
+ *     description: This api is used to retrieved a Policy object given its policy ID
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: policyId
+ *         description: policyId of the policy to be searched for
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Policy has been found
+ *       404:
+ *         description: Policy has not been found
+ */
 app.get(
   config.app.api.getPolicy.path.replace("{policyId}", ":policyId"),
   async (req, res) => {
@@ -110,6 +137,33 @@ app.get(
   }
 );
 
+/**
+ * @swagger
+ * /flights/search:
+ *   get:
+ *     description: This api is used to search for flights based on the search params
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: from
+ *         description: departure airport AITA code
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: to
+ *         description: arrival airport AITA code
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: date
+ *         description: date in YYYY-MM-DD format
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Flights satisying the search criterias will be returned
+ */
 app.get(config.app.api.getFlightsByRoute.path, async (req, res) => {
   const from = req.query.from;
   const to = req.query.to;
@@ -122,6 +176,33 @@ app.get(config.app.api.getFlightsByRoute.path, async (req, res) => {
   res.status(200).send(results);
 });
 
+/**
+ * @swagger
+ * /flight/stats:
+ *   get:
+ *     description: This api is used to get the latest delay/cancellation stats of a flight
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: from
+ *         description: departure airport AITA code
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: fsCode
+ *         description: fsCode of the flight
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: carrierCode
+ *         description: carrierCode of the flight
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Recent stats of the flight
+ */
 app.get(config.app.api.getFlightStats.path, async (req, res) => {
   const from = req.query.from;
   const fsCode = req.query.fsCode;
@@ -132,11 +213,60 @@ app.get(config.app.api.getFlightStats.path, async (req, res) => {
   res.status(200).send(results);
 });
 
+/**
+ * @swagger
+ * /airport/delay:
+ *   get:
+ *     description: This api is used to get the latest delay rating of an airport
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: airport
+ *         description: airport AITA code
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: returns the latest delay airport of the airport
+ */
 app.get(config.app.api.getDelayByAirport.path, async (req, res) => {
   const results = await Flyt.getDelayByAirport(req.query.airport);
   res.status(200).send(results);
 });
 
+/**
+ * @swagger
+ * /premium:
+ *   get:
+ *     description: This api is used to retrieve the premium to be paid based on search params
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: policyId
+ *         description: policyId of the policy for which the premium is been calculated
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: from
+ *         description: departure airport AITA code
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: fsCode
+ *         description: fsCode of the flight
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: carrierCode
+ *         description: carrierCode of the flight
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Calculate the premium for the policy and returns. Keep retrying until premium is 0
+ */
 app.get(config.app.api.getPremium.path, async (req, res) => {
   const policyId = req.query.policyId;
   const from = req.query.from;
@@ -146,6 +276,8 @@ app.get(config.app.api.getPremium.path, async (req, res) => {
   const premium = await Flyt.getPremium(policyId, from, fsCode, carrierCode);
   res.status(200).send({premium});
 });
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(Swagger));
 
 const port = !_.isUndefined(process.env.PORT) ? process.env.PORT : 4000;
 app.listen(port, () => {
