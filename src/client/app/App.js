1dd3d98b9d65355b0036487ce901568c4e6a8aab
Robin Thomas <robinthomas2591@gmail.com>
2019-10-31 22:10:58 +0800

    Update UI

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -11,7 +11,7 @@ const App = () => (
     <div className="stepper-form-container">
       <EmptyRow height="50px" />
       <Stepper />
-      <EmptyRow height="100px" />
+      <EmptyRow height="80px" />
       <Footer />
     </div>
   </div>
