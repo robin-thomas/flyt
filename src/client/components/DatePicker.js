c50e1cc474ec0dafbc4c1f604ac8d73124e7494c
Robin Thomas <robinthomas2591@gmail.com>
2019-11-01 08:01:10 +0800

    Restrict the days

--- src/client/components/DatePicker.js
+++ src/client/components/DatePicker.js
@@ -1,14 +1,16 @@
-import React, { forwardRef } from "react";
+import React, {forwardRef} from "react";
 
 import DatePicker from "react-datepicker";
-import { MDBInput } from "mdbreact";
+import {MDBInput} from "mdbreact";
+
 import format from "date-fns/format";
+import addDays from "date-fns/addDays";
 
 import "react-datepicker/dist/react-datepicker.min.css";
 
 import "./DatePicker.css";
 
-const Calendar = forwardRef(({ label, date, setDate, onClick }, ref) => (
+const Calendar = forwardRef(({label, date, setDate, onClick}, ref) => (
   <MDBInput
     ref={ref}
     label={label}
@@ -17,12 +19,13 @@ const Calendar = forwardRef(({ label, date, setDate, onClick }, ref) => (
   />
 ));
 
-const ScheduleDate = ({ date, setDate, label }) => (
+const ScheduleDate = ({date, setDate, label}) => (
   <DatePicker
     selected={date}
     onChange={setDate}
     withPortal
     minDate={new Date()}
+    maxDate={addDays(new Date(), 7)}
     onClickOutside={() => document.activeElement.blur()}
     customInput={<Calendar date={date} setDate={setDate} label={label} />}
   />
